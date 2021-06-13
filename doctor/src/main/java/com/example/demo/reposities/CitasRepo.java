315888bde516c0a0499675bb55b7cf87a5f34cf4
unknown <pjuarezp2@miumg.edu.gt>
2021-06-13 01:22:44 -0600

    esamen final

---
+++ doctor/src/main/java/com/example/demo/reposities/CitasRepo.java
@@ -0,0 +1,10 @@
+
+package com.example.demo.reposities;
+
+import org.springframework.data.repository.CrudRepository;
+
+import  com.example.demo.entities.CitasEntitis;
+
+public interface CitasRepo extends CrudRepository<CitasEntitis, Long>{
+
+}
